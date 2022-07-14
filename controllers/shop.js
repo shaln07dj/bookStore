edb1aebab5f884de483fd6307dae9812be3b6810
shalnsingh@outlook.com <shalnsingh@outlook.com>
2022-07-14 14:16:50 +0530

    updating shop controller

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require('../models/product');
-
+const Cart = require('../models/cart');
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/product-list', {
@@ -13,10 +13,14 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId, product=> {
-    console.log(product)
+   res.render('shop/product-detail', {
+    product:product,
+    pageTitle:product.title,
+  path:'/products'
+})
   });
 
-  res.redirect('/');
+ 
 }
 
 exports.getIndex = (req, res, next) => {
@@ -36,6 +40,13 @@ exports.getCart = (req, res, next) => {
   });
 };
 
+exports.postCart = ( req, res,next) => {
+  const prodId=req.body.productId;
+  Product.findById(prodId,(product) =>{
+    Cart.addProduct(prodId,product.price)
+  });
+  res.redirect('/cart');
+}
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
     path: '/orders',
